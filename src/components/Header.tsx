88f37808338d48fe637c32246478807d659e4682
Mick McGuinness <mick.mcguinness@applicationperformance.com>
2025-03-18 18:47:51 +0000

    Update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,31 +11,23 @@ const Header = ({ onOpenZipFile = () => {} }: HeaderProps) => {
   return (
     <header className="w-full h-20 bg-[#02203E] dark:bg-[#02203E] border-b border-slate-800 flex items-center justify-between px-6 shadow-md">
       <div className="flex items-center gap-3">
-        <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#e11d48"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="w-8 h-8"
-          >
-            <path
-              d="M6.5 12c.94-3.46 4.94-6 8.5-6 0 0 3 0 3 3s-2 3-2 3 1-1 1-3c0-3-7-3-10 6-3-9-10-9-10-6 0 2 1 3 1 3s-2 0-2-3 3-3 3-3c3.56 0 7.56 2.54 8.5 6"
-              fill="#e11d48"
-              fillOpacity="0.2"
-            />
-            <circle cx="19" cy="11.5" r="0.5" fill="#000" />
-            <path d="M2 9c-1 1-1.5 3-1.5 3s0.5 2 1.5 3" />
-            <path d="M10 8c0 0 0.5-1.5 1.5-1.5" />
-            <path d="M10 16c0 0 0.5 1.5 1.5 1.5" />
-            <path d="M18 12v.5" />
-            <path d="M18 13.5v.5" />
-          </svg>
+        <div className="flex items-center flex gap-4">
+          <img
+            src="/fish-icon2.svg"
+            alt="Red Snapper logo"
+            className="w-6 h-6 dark:hidden"
+          />
+          <img
+            src="/fish-icon2-white.svg"
+            alt="Red Snapper logo"
+            className="w-6 h-6 hidden dark:block"
+          />
+          <img
+            src="/red-snapper-badge.svg"
+            alt="Red Snapper badge"
+            className="w-24 h-10"
+          />
         </div>
-        <h1 className="text-2xl font-bold text-white">Red Snapper</h1>
         <ThemeToggle />
       </div>
 
